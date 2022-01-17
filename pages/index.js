bf11314f617526c5650dd9419ed1bb476061ba79
giornn0 <shaaamsuu@gmail.com>
2022-01-17 14:14:16 -0300

    fixes

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import Page from '../components/Page'
 export default function Home() {
   return (
     <>
-    <Script src="https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyA-XXd7cuH5fGHy1yQdN4Qj4VOaZwDuHAU" strategy="beforeInteractive"/>
+    <Script src={`https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}" strategy="beforeInteractive`}/>
     <Page></Page>
     </>
   )
